9c82ad01c57ad6494c167c1ddbb67199bebfae90
jeferson <jsmartins959@gmail.com>
2026-03-23 16:09:19 -0300

    Project Created

---
+++ backend/src/main/java/com/jeferson/jecatalog/JecatalogApplication.java
@@ -0,0 +1,13 @@
+package com.jeferson.jecatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JecatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JecatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jeferson/jecatalog/JecatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeferson.jecatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JecatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
