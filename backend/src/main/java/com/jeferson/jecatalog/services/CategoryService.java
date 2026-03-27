4a613f3818bb7eb650a1b1e075f4a9dc60fe7f33
jeferson <jsmartins959@gmail.com>
2026-03-26 23:11:14 -0300

    feat: DatabaseException and Delete Category

--- backend/src/main/java/com/jeferson/jecatalog/services/CategoryService.java
+++ backend/src/main/java/com/jeferson/jecatalog/services/CategoryService.java
@@ -5,13 +5,17 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jeferson.jecatalog.dto.CategoryDTO;
 import com.jeferson.jecatalog.entities.Category;
 import com.jeferson.jecatalog.repositories.CategoryRepository;
-import com.jeferson.jecatalog.services.exceptions.EntityNotFoundException;
+import com.jeferson.jecatalog.services.exceptions.DatabaseException;
+import com.jeferson.jecatalog.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -28,7 +32,40 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional <Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category entity = repository.getReferenceById(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new CategoryDTO(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id noy found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		if( !repository.existsById(id)) {
+			throw new ResourceNotFoundException("Resource not found");
+		}
+		try {
+			repository.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
 	}
 }
---
+++ backend/src/main/java/com/jeferson/jecatalog/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.jeferson.jecatalog.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(String msg) {
+		super(msg);
+	}
+}
